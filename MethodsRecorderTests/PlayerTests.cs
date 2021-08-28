08535c2d76295fae46a27a3a5a97213b78ecad0e
lskrzypek <lskrzypek@op.pl>
2021-08-28 12:58:33 +0200

    Player tests

---
+++ MethodsRecorderTests/PlayerTests.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MethodsRecorder;
+using MethodsRecorderTests.ExampleData.Persons;
+using System.Linq;
+
+namespace MethodsRecorderTests
+{
+    [TestClass]
+    public class PlayerTests
+    {
+        [TestMethod]
+        public void Example_Player_Method_without_parameter_and_simple_return_type()
+        {
+            var player = new Player("test.txt");
+
+            var personsPlayerDao = player.CreatePlayingObject<IPersonsDao>().Object;
+            var count = personsPlayerDao.GetCount();
+
+            var expectedCount = new PersonsDao(new PersonsReader()).GetCount();
+            Assert.AreEqual(expectedCount, count);
+        }
+
+        [TestMethod]
+        public void Example_Player_Method_without_parameter_and_complex_return_type()
+        {
+            var player = new Player("test.txt");
+
+            var personsPlayerDao = player.CreatePlayingObject<IPersonsDao>().Object;
+            var persons = personsPlayerDao.GetAllPersons();
+
+            var expectedPersons = new PersonsDao(new PersonsReader()).GetAllPersons();
+            Assert.AreEqual(expectedPersons.Count(), persons.Count());
+        }
+
+        [TestMethod]
+        public void Example_Player_Method_with_parameters_and_complex_return_type()
+        {
+            var player = new Player("test.txt");
+
+            var personsPlayerDao = player.CreatePlayingObject<IPersonsDao>().Object;
+            var person = personsPlayerDao.GetOne("Jan", "Kowalski");
+
+            var expectedPerson = new PersonsDao(new PersonsReader()).GetOne("Jan", "Kowalski");
+            Assert.AreEqual(expectedPerson.LastName, person.LastName);
+        }
+    }
+}
