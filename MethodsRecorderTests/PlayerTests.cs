7fd47cccf9e78d7419c6e9d5bf795e8218ac6e90
lskrzypek <lskrzypek@op.pl>
2021-08-28 21:30:36 +0200

    bug in tests fixes

--- MethodsRecorderTests/PlayerTests.cs
+++ MethodsRecorderTests/PlayerTests.cs
@@ -11,37 +11,47 @@ namespace MethodsRecorderTests
         [TestMethod]
         public void Example_Player_Method_without_parameter_and_simple_return_type()
         {
-            var player = new Player("test.txt");
+            var player = this.CreatePersonsPlayer();
 
             var personsPlayerDao = player.CreatePlayingObject<IPersonsDao>().Object;
             var count = personsPlayerDao.GetCount();
 
-            var expectedCount = new PersonsDao(new PersonsReader()).GetCount();
+            var expectedCount = this.CreatePersonsDao().GetCount();
             Assert.AreEqual(expectedCount, count);
         }
 
         [TestMethod]
         public void Example_Player_Method_without_parameter_and_complex_return_type()
         {
-            var player = new Player("test.txt");
+            var player = this.CreatePersonsPlayer();
 
             var personsPlayerDao = player.CreatePlayingObject<IPersonsDao>().Object;
             var persons = personsPlayerDao.GetAllPersons();
 
-            var expectedPersons = new PersonsDao(new PersonsReader()).GetAllPersons();
+            var expectedPersons = this.CreatePersonsDao().GetAllPersons();
             Assert.AreEqual(expectedPersons.Count(), persons.Count());
         }
 
         [TestMethod]
         public void Example_Player_Method_with_parameters_and_complex_return_type()
         {
-            var player = new Player("test.txt");
+            var player = this.CreatePersonsPlayer();
 
             var personsPlayerDao = player.CreatePlayingObject<IPersonsDao>().Object;
             var person = personsPlayerDao.GetOne("Jan", "Kowalski");
 
-            var expectedPerson = new PersonsDao(new PersonsReader()).GetOne("Jan", "Kowalski");
+            var expectedPerson = this.CreatePersonsDao().GetOne("Jan", "Kowalski");
             Assert.AreEqual(expectedPerson.LastName, person.LastName);
+        }
+
+        private PersonsDao CreatePersonsDao()
+        {
+            return new PersonsDao(new PersonsReader());
+        }
+
+        private Player CreatePersonsPlayer()
+        {
+            return new Player("TestFiles/persons.txt");
         }
     }
 }
